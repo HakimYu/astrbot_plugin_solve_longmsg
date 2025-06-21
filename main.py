abc8f88f0c33490a607b8566464f9bd6982f0446
HakimYu <yhjredin@gmail.com>
2025-06-21 18:13:35 +0800

    添加处理群成员和自身消息的配置选项，添加处理自身消息功能

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageChain
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger, AstrBotConfig
-from astrbot.api.message_components import Node, Plain, Nodes
+from astrbot.api.message_components import Node
 from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import AiocqhttpMessageEvent
 
 
-@register("solve_longmsg", "HakimYu", "检测并处理长消息", "1.0.2")
+@register("solve_longmsg", "HakimYu", "撤回并合并转发群员或者机器人的长消息", "1.1.0")
 class LongMessageHandler(Star):
     def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
         self.config = config
 
+    async def _send_forwarded_message(self, event: AstrMessageEvent, name: str, id: str, message: list):
+        """发送只包含一个人的合并转发消息"""
+        if isinstance(message, list):
+            nodes = []
+            nodes.append(Node(
+                            uin=id,
+                            name=name,
+                            content=message
+                        ))
+            await event.send(event.chain_result(nodes))
 
     @filter.event_message_type(filter.EventMessageType.GROUP_MESSAGE)
     async def handle_message(self, event: AstrMessageEvent):
         """处理所有消息，检测长度并处理"""
+        assert self.config.solve_group_member
         group_id = event.get_group_id()
         if self.config.group_whitelist and group_id not in self.config.group_whitelist:
             return
 
         message_str = event.message_str
         if len(message_str) > self.config.max_length:
-            # 获取发送者信息
-            sender_name = event.get_sender_name()
-            sender_id = event.get_sender_id()
             # 撤回原消息
             if event.get_platform_name() == "aiocqhttp":
                 assert isinstance(event, AiocqhttpMessageEvent)
@@ -35,10 +43,15 @@ class LongMessageHandler(Star):
                     return
 
                 # 发送合并转发消息
-                node = Node(
-                    uin=sender_id,
-                    name=sender_name,
-                    content=[Plain(message_str)]
-                )
-                await event.send(event.chain_result([node]))
+                await self._send_forwarded_message(event, event.get_sender_name(), event.get_sender_id(), event.get_messages())
                 event.stop_event()
+
+    @filter.on_decorating_result()
+    async def on_decorating_result(self, event: AstrMessageEvent):
+        assert self.config.solve_self_message
+        result = event.get_result()
+        chain = result.chain
+        if len(result.get_plain_text()) > self.config.max_length:
+            await self._send_forwarded_message(event, "", event.get_self_id(), chain)
+            chain.clear()
+            event.stop_event()
