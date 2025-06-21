449d49de7429736e7388bb75277c486676e0cee4
HakimYu <yhjredin@gmail.com>
2025-06-21 19:57:22 +0800

    储存MessageChain以修复超长消息llm不回复的问题

--- main.py
+++ main.py
@@ -2,30 +2,22 @@ from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger, AstrBotConfig
 from astrbot.api.message_components import Node
+from astrbot.core.message.message_event_result import MessageChain
 from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import AiocqhttpMessageEvent
 
 
-@register("solve_longmsg", "HakimYu", "撤回并合并转发群员或者机器人的长消息", "1.1.0")
+@register("solve_longmsg", "HakimYu", "撤回并合并转发群员或者机器人的长消息", "1.1.1")
 class LongMessageHandler(Star):
     def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
         self.config = config
-
-    async def _send_forwarded_message(self, event: AstrMessageEvent, name: str, id: str, message: list):
-        """发送只包含一个人的合并转发消息"""
-        if isinstance(message, list):
-            nodes = []
-            nodes.append(Node(
-                            uin=id,
-                            name=name,
-                            content=message
-                        ))
-            await event.send(event.chain_result(nodes))
+        self.message_chain = None
 
     @filter.event_message_type(filter.EventMessageType.GROUP_MESSAGE)
     async def handle_message(self, event: AstrMessageEvent):
         """处理所有消息，检测长度并处理"""
-        assert self.config.solve_group_member
+        if not self.config.solve_group_member:
+            return
         group_id = event.get_group_id()
         if self.config.group_whitelist and group_id not in self.config.group_whitelist:
             return
@@ -40,18 +32,27 @@ class LongMessageHandler(Star):
                     await client.delete_msg(message_id=int(event.message_obj.message_id))
                 except Exception as e:
                     logger.info(f"消息撤回失败: {e}，可能已被手动撤回，取消转发。")
-                    return
 
-                # 发送合并转发消息
-                await self._send_forwarded_message(event, event.get_sender_name(), event.get_sender_id(), event.get_messages())
-                event.stop_event()
+                # 储存消息链
+                self.message_chain = MessageChain([Node(
+                    uin=event.get_sender_id(),
+                    name=event.get_sender_name(),
+                    content=event.get_messages()
+                )])
 
     @filter.on_decorating_result()
     async def on_decorating_result(self, event: AstrMessageEvent):
-        assert self.config.solve_self_message
+        if self.message_chain:
+            await event.send(self.message_chain)
+            self.message_chain = None
         result = event.get_result()
         chain = result.chain
+        if not self.config.solve_self_message:
+            return
         if len(result.get_plain_text()) > self.config.max_length:
-            await self._send_forwarded_message(event, "", event.get_self_id(), chain)
+            await event.send(MessageChain([Node(
+                uin=event.get_self_id(),
+                name="",
+                content=chain
+            )]))
             chain.clear()
-            event.stop_event()
